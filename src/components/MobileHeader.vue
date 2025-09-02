81bd46ea720d0c811db00b7e0d9f6a666371ac76
Winzlieb <winzlieb@c3d2.de>
2025-09-02 07:17:18 +0200

    Add FilterDropdownButton to MobileHeader and SearchbarMenuItem
    Signed-off-by: Winzlieb <winzlieb@c3d2.de>

--- src/components/MobileHeader.vue
+++ src/components/MobileHeader.vue
@@ -12,6 +12,7 @@
 
     <div class="actions">
       <UploadMenuItem />
+      <FilterDropdownButton />
       <SearchbarMenuItem />
     </div>
   </div>
@@ -22,6 +23,7 @@ import { defineComponent } from 'vue';
 import { generateUrl } from '@nextcloud/router';
 
 import UploadMenuItem from '@components/header/UploadMenuItem.vue';
+import FilterDropdownButton from '@components/FilterDropdownButton.vue';
 import SearchbarMenuItem from '@components/header/SearchbarMenuItem.vue';
 import XImg from '@components/frame/XImg.vue';
 
@@ -33,6 +35,7 @@ export default defineComponent({
   name: 'MobileHeader',
   components: {
     UploadMenuItem,
+    FilterDropdownButton,
     SearchbarMenuItem,
     XImg,
   },
