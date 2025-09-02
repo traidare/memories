ba84a1c27dfac582dda2c8d43bb86c083fcb7f5b
Winzlieb <winzlieb@c3d2.de>
2025-09-02 15:22:54 +0200

    Add metadata options for slideshow and gallery
    Signed-off-by: Winzlieb <winzlieb@c3d2.de>

--- lib/Controller/OtherController.php
+++ lib/Controller/OtherController.php
@@ -110,6 +110,9 @@ final class OtherController extends GenericApiController
                 'livephoto_loop' => 'true' === $getAppConfig('livephotoLoop', 'false'),
                 'video_loop' => 'true' === $getAppConfig('videoLoop', 'false'),
                 'sidebar_filepath' => 'true' === $getAppConfig('sidebarFilepath', 'false'),
+                'metadata_in_slideshow' => 'true' === $getAppConfig('metadataInSlideshow', 'false'),
+                'metadata_in_gallery' => 'true' === $getAppConfig('metadataInGallery', 'false'),
+                'enable_exif_photo_rating_in_gallery' => 'true' === $getAppConfig('enableExifPhotoRatingInGallery', 'false'),
 
                 // folder settings
                 'folders_path' => $getAppConfig('foldersPath', '/'),
