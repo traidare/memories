f0aa7483fa7ceff0798e6d4e375be9169cdf4619
Winzlieb <winzlieb@c3d2.de>
2025-09-03 10:46:34 +0200

    Enhance FilterComponent to support collaborative tags filtering
    - Added a new property `showCollaborativeTagsFilter` to conditionally display the tags filter section.
    - Updated the `hasActiveFilters` computed property to account for collaborative tags when determining active filters.
    
    Signed-off-by: Winzlieb <winzlieb@c3d2.de>

--- src/components/MobileHeader.vue
+++ src/components/MobileHeader.vue
@@ -12,7 +12,6 @@
 
     <div class="actions">
       <UploadMenuItem />
-      <FilterDropdownButton />
       <SearchbarMenuItem />
     </div>
   </div>
@@ -23,7 +22,6 @@ import { defineComponent } from 'vue';
 import { generateUrl } from '@nextcloud/router';
 
 import UploadMenuItem from '@components/header/UploadMenuItem.vue';
-import FilterDropdownButton from '@components/FilterDropdownButton.vue';
 import SearchbarMenuItem from '@components/header/SearchbarMenuItem.vue';
 import XImg from '@components/frame/XImg.vue';
 
@@ -35,7 +33,6 @@ export default defineComponent({
   name: 'MobileHeader',
   components: {
     UploadMenuItem,
-    FilterDropdownButton,
     SearchbarMenuItem,
     XImg,
   },
