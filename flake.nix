48a6896f57e631e2ba45769357763115a5320856
traidare <traidare@proton.me>
2026-03-28 13:53:44 +0100

    Add Nix flake with packaging

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} ({lib, ...}: {
+      systems = ["x86_64-linux" "aarch64-linux"];
+
+      perSystem = {
+        config,
+        inputs',
+        pkgs,
+        ...
+      }: {
+        packages = {
+          nextcloud-app-memories = pkgs.callPackage ./nix/package.nix {
+            inherit (inputs'.gomod2nix.legacyPackages) buildGoApplication;
+          };
+          default = config.packages.nextcloud-app-memories;
+        };
+      };
+    });
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
